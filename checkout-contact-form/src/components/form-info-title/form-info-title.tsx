2a0eb1581b8193ba920f9ac4d160403337265022
Rony Antolinez <rony1710.dev@gmail.com>
2025-08-05 03:35:25 -0500

    add: form section title

---
+++ checkout-contact-form/src/components/form-info-title/form-info-title.tsx
@@ -0,0 +1,10 @@
+import styles from "./form-info-title-styles.module.css";
+
+export const FormInfoTitle = () => {
+  return (
+    <div className={styles['form-info-title-container']}>
+      <p>We're almost there...</p>
+      <p>Update your contact data</p>
+    </div>
+  );
+};
--- checkout-contact-form/src/pages/checkout-info/checkout-info.tsx
+++ checkout-contact-form/src/pages/checkout-info/checkout-info.tsx
@@ -1,16 +1,14 @@
 import { Navbar } from '@/components/navbar/navbar';
 import styles from './checkout-info-styles.module.css';
 import { FormInfoTemplate } from '@/templates/form-info-template/form-info-template';
+import { FormInfoTitle } from '@/components/form-info-title/form-info-title';
 
 export const CheckoutInfo = () => {
   return (
     <section className={styles['checkout-info-container']}>
       <Navbar />
 
-      <div>
-        <p>We're almost there</p>
-        <p>Update your contact data</p>
-      </div>
+      <FormInfoTitle />
 
       <FormInfoTemplate />
     </section>
