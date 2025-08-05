6b47645cd51f2c62f886120fe0881be13b35c9ca
Rony Antolinez <rony1710.dev@gmail.com>
2025-08-05 02:41:27 -0500

    add: input component file

--- checkout-contact-form/src/App.tsx
+++ checkout-contact-form/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import { TestComponent } from '@/components/test-component/test-component'
+import { Input } from '@/components/input/input'
 
 function App() {
 
   return (
     <>
-<TestComponent />
+      <TestComponent />
+      <Input />
     </>
   )
 }
