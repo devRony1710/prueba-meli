1fc2b67320f280093cd38a8e817d307a09d59bda
Rony Antolinez <rony1710.dev@gmail.com>
2025-08-06 10:35:47 -0500

    change: animatio when it's not focused

--- checkout-contact-form/src/components/input/input.tsx
+++ checkout-contact-form/src/components/input/input.tsx
@@ -10,7 +10,7 @@ export const Input: FC<InputProps> = ({ label, name, type }) => {
   return (
     <div className={styles['input-container']}>
       <label
-        className={`${styles['label']} ${inputValue || isFocused ? styles['focused'] : ''}`}
+        className={`${styles['label']} ${inputValue || isFocused ? styles['focused'] : styles['not-focused']}`}
         htmlFor={name}
       >
         {label}
