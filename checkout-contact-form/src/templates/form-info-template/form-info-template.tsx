19bfe65d739c7e2e65533cc2adaa7e57ffed8417
Rony Antolinez <rony1710.dev@gmail.com>
2025-08-06 11:23:40 -0500

    add: captcha config

--- checkout-contact-form/src/templates/form-info-template/form-info-template.tsx
+++ checkout-contact-form/src/templates/form-info-template/form-info-template.tsx
@@ -1,10 +1,21 @@
 import { Input } from '@/components/input/input';
 import styles from './form-info-template-styles.module.css';
 import { Selector } from '@/components/selector/selector';
+import ReCAPTCHA from 'react-google-recaptcha';
+import { useRef } from 'react';
 
 export const FormInfoTemplate = () => {
+const recaptchaRef = useRef<ReCAPTCHA>(null);
+console.log(recaptchaRef);
+  
   const handleOnSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+    const captchaValue = recaptchaRef.current?.getValue();
+    console.log(captchaValue);
+    if (!captchaValue) {
+      console.log('Please verify the reCAPTCHA');
+      return;
+    }
     console.log('Form submitted');
   };
 
@@ -14,6 +25,9 @@ export const FormInfoTemplate = () => {
       <Selector />
       <Input label="Address" name="address" type="text" />
       <Input label="Phone" name="phone" type="text" maxLength={10}  />
+      <ReCAPTCHA sitekey={import.meta.env.VITE_RECAPTCHA_SITE_KEY} ref={recaptchaRef} />
+
+      <button type="submit">Submit</button>
     </form>
   );
 };
