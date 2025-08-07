b25b77c43f70f6a1ee1cd12249befff0f32a42d7
Rony Antolinez <rony1710.dev@gmail.com>
2025-08-07 14:26:01 -0500

    add: translations for not found

--- checkout-contact-form/src/pages/404/404.tsx
+++ checkout-contact-form/src/pages/404/404.tsx
@@ -1,8 +1,19 @@
+import { Navbar } from "@/components/navbar/navbar";
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
+import { Button } from "@/components/button/button";
+
 export const NotFound = () => {
+    const { t } = useTranslation();
+    const navigate = useNavigate();
     return (
         <section>
-            <h1>404</h1>
-            <p>Page not found</p>
+            <Navbar />
+            <div style={{padding: '2rem'}}>
+                <h1>404</h1>
+                <p>{t('pageNotFound')}</p>
+                <Button label={t('backToHome')} onClick={() => navigate('/')} />
+            </div>
         </section>
     );
 };
--- checkout-contact-form/src/pages/checkout-success/checkout-success.tsx
+++ checkout-contact-form/src/pages/checkout-success/checkout-success.tsx
@@ -1,10 +1,19 @@
 import { Navbar } from '@/components/navbar/navbar';
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+import { Button } from '@/components/button/button';
 
 export const CheckoutSuccess = () => {
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+
   return (
     <section>
       <Navbar />
-      <h1>Checkout Success</h1>
+      <div style={{padding: '2rem'}}>
+        <h1>{t('checkoutSuccessTitle')}</h1>
+        <Button label={t('checkoutSuccessButton')} onClick={() => navigate('/')} />
+      </div>
     </section>
   );
 };
