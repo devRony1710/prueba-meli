77b0d2bb067d97dee663ab1175f98f6f38ad3179
Rony Antolinez <rony1710.dev@gmail.com>
2025-08-07 11:59:00 -0500

    add: previous page for simulate interaction

---
+++ checkout-contact-form/src/pages/previous-page/previous-page.tsx
@@ -0,0 +1,24 @@
+import { Navbar } from '@/components/navbar/navbar';
+import styles from './previous-page-styles.module.css';
+import { Button } from '@/components/button/button';
+import { useNavigate } from 'react-router-dom';
+
+export const PreviousPage = () => {
+  const navigate = useNavigate();
+
+  const navigateToCheckout = () => {
+    navigate('/checkout?referrer=previous-page&token=123456789');
+  };
+  
+  return (
+    <section className={styles['previous-page-container']}>
+      <Navbar />
+
+      <div className={styles['previous-page-content-wrapper']}>
+        <h1 className={styles['previous-page-title']}>Previous Page</h1>
+
+        <Button label="Go to Checkout" onClick={navigateToCheckout} />
+      </div>
+    </section>
+  );
+};
--- checkout-contact-form/src/routes/app-routes.tsx
+++ checkout-contact-form/src/routes/app-routes.tsx
@@ -1,20 +1,22 @@
-import { BrowserRouter, useRoutes } from "react-router-dom";
-import { CheckoutInfo } from "../pages/checkout-info/checkout-info";
-import { NotFound } from "../pages/404/404";
+import { BrowserRouter, useRoutes } from 'react-router-dom';
+import { CheckoutInfo } from '../pages/checkout-info/checkout-info';
+import { NotFound } from '../pages/404/404';
+import { PreviousPage } from '../pages/previous-page/previous-page';
 
 const AppRoutes = () => {
-    const routes = useRoutes([
-        { path: "/", element: <CheckoutInfo /> },
-        { path: "*", element: <NotFound /> },
-    ]);
+  const routes = useRoutes([
+    { path: '/', element: <PreviousPage /> },
+    { path: '/checkout', element: <CheckoutInfo /> },
+    { path: '*', element: <NotFound /> },
+  ]);
 
-    return routes;
+  return routes;
 };
 
 export const App = () => {
-    return (
-        <BrowserRouter>
-            <AppRoutes />
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
 };
