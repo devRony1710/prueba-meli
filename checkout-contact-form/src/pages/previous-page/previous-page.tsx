178f9f3c92f25bd59e397c19554ce0a481d6483d
Rony Antolinez <rony1710.dev@gmail.com>
2025-08-07 14:21:54 -0500

    add: translations for previous-page

--- checkout-contact-form/src/pages/previous-page/previous-page.tsx
+++ checkout-contact-form/src/pages/previous-page/previous-page.tsx
@@ -2,9 +2,11 @@ import { Navbar } from '@/components/navbar/navbar';
 import styles from './previous-page-styles.module.css';
 import { Button } from '@/components/button/button';
 import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 export const PreviousPage = () => {
   const navigate = useNavigate();
+  const { t } = useTranslation();
 
   const navigateToCheckout = () => {
     navigate('/checkout?referrer=previous-page&token=123456789');
@@ -19,10 +21,10 @@ export const PreviousPage = () => {
       <Navbar />
 
       <div className={styles['previous-page-content-wrapper']}>
-        <h1 className={styles['previous-page-title']}>Previous Page</h1>
+        <h1 className={styles['previous-page-title']}>{t('previousPageTitle')}</h1>
 
-        <Button label="Go to Checkout" onClick={navigateToCheckout} />
-        <Button label="Go to Checkout User" onClick={navigateToCheckoutUserMock} />
+        <Button label={t('previousPageButtonGoToCheckout')} onClick={navigateToCheckout} />
+        <Button label={t('previousPageButtonGoToCheckoutUser')} onClick={navigateToCheckoutUserMock} />
       </div>
     </section>
   );
